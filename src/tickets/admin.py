ee9e2cf5153cfda46e694352fbdcffb5aed31524
Jared AHaza <jaredahazq@gmail.com>
2023-01-24 09:04:49 +0300

    Created and registred Atendee's model

--- src/tickets/admin.py
+++ src/tickets/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.models import AtendeeUser
 
 # Register your models here.
+admin.site.site_header = 'LTTP Admin Panel'
+
+
+@admin.register(AtendeeUser)
+class AtendeeUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'phone')
+    search_fields = ('username', 'email')
+    list_per_page = 10
