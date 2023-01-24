fe03b474be920ab4c03c6370a6b7319ddbeef841
Jared AHaza <jaredahazq@gmail.com>
2023-01-24 09:12:09 +0300

    Created Home view and registred atendeeuser view

--- src/tickets/admin.py
+++ src/tickets/admin.py
@@ -10,3 +10,4 @@ class AtendeeUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'phone')
     search_fields = ('username', 'email')
     list_per_page = 10
+
--- src/tickets/views.py
+++ src/tickets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from app.models import AtendeeUser
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from app.forms import TrainForm
+from datetime import timezone, datetime, timedelta
+
 
 # Create your views here.
+
+# homepage view
+
+class Home(View):
+    def get(self, request):
+        form = EventForm
+        return render(request, 'home.html', {'form': form})
