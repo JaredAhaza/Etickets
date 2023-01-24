552b90a47a2cb697cdd44236c38e77e1b6e32895
Jared AHaza <jaredahazq@gmail.com>
2023-01-24 09:44:27 +0300

    Registred all models in the views

--- src/tickets/views.py
+++ src/tickets/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from app.models import AtendeeUser
+from app.models import AtendeeUser, Location, TicketType, Event, Booking
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from app.forms import TrainForm
+from app.forms import EventForm
 from datetime import timezone, datetime, timedelta
 
 
